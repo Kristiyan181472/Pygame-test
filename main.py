5731cc44a9e22ba1f6f38d43ac83f809da673ba0
Kristiyan181472 <61007427+Kristiyan181472@users.noreply.github.com>
2024-01-11 23:01:01 +0200

    aa
    aaa

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame, sys
+from settings import *
+#Here we all atributes in the class we use it to define what operations can be done my a variable
+class Game:
+    def __init__ (self):
+        #here we initiate pygame
+        pygame.init()
+        #display serfece that is the basic setup we need for pigame
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+          while True:
+            for event in pygame.event.get():
+                pygame.quit()
+                sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+if __name__ == '__main__':
+    game=Game()
+    game.run()
+    
